16660e0a1e6319f4d141f39762e129aca658fc61
Josh Clark <josh@goclarks.com>
2016-05-24 11:28:57 -0500

    Initial Checkin

---
+++ src/EncodeDotNet/Encode.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EncodeDotNet
+{
+    public static class Encode
+    {
+        private static readonly char[] _encodeMap = "0123456789ABCDEFGHJKMNPQRSTVWXYZ".ToArray();
+        private static readonly byte[] _decodeMap = GenerateDecodeMap(_encodeMap);
+        private const byte UnusedEntry = 0xFF;
+        private const long Largest12DigitValue = 0x0FFFFFFFFFFFFFFF;
+
+        private static byte[] GenerateDecodeMap(char[] encodeMap)
+        {
+            int size = 'z' + 1;
+            var result = new byte[size];
+            for (int i = 0; i < result.Length; ++i)
+            {
+                result[i] = UnusedEntry;
+            }
+
+            // Add a lookup for both upper and lower case letters
+            for (byte i = 0; i < encodeMap.Length; ++i)
+            {
+                result[encodeMap[i]] = i;
+                result[char.ToLowerInvariant(encodeMap[i])] = i;
+            }
+
+            // Special cases for characters that look like numbers.
+            result['O'] = 0;
+            result['o'] = 0;
+            result['I'] = 1;
+            result['i'] = 1;
+            result['L'] = 1;
+            result['l'] = 1;
+
+            return result;
+        }
+
+
+
+
+
+        public static string ToBase32(long number)
+        {
+            return ToBase32(number, true);
+        }
+        public static string ToBase32(long number, bool includeSeperator)
+        {
+            int size = 12 + (includeSeperator ? 2 : 0);
+            size += (number > Largest12DigitValue) ? 1 : 0;
+
+            char[] result = new char[size];
+
+            int index = size - 1;
+            do
+            {
+                if (includeSeperator && ((index == 10) || (index == 4)))
+                {
+                    result[index--] = '-';
+                }
+
+                result[index--] = _encodeMap[number & 0x1F];
+                number >>= 5;
+            } while (index >= 0);
+
+            var id = new string(result, 0, size);
+            return id;
+        }
+
+        public static long FromBase32(string str)
+        {
+            if (str == null)
+                throw new ArgumentNullException();
+
+            const int baseForDecoding = 32;
+            const int maxAllowedEncodedDigits = 12;
+
+
+            long value = 0;
+            long currentBase = 1;
+            int encodedDigitCount = 0;
+
+            for (int i = str.Length - 1; i >= 0; --i)
+            {
+                char currentDigit = str[i];
+                if (currentDigit == '-')
+                    continue;
+
+                byte decodedValue = currentDigit < _decodeMap.Length ? _decodeMap[currentDigit] : UnusedEntry;
+
+                if (decodedValue == UnusedEntry)
+                    throw new FormatException($"Illegal character '{currentDigit}' in encoded string '{str}'");
+
+                
+                if ((encodedDigitCount > maxAllowedEncodedDigits) ||
+                    ((encodedDigitCount == maxAllowedEncodedDigits) && (decodedValue > 7)))
+                {
+                    throw new ArgumentOutOfRangeException($"Encoded string '{str}' is too large. It won't fit in a long.");
+                }
+
+                value += decodedValue * currentBase;
+                currentBase *= baseForDecoding;
+                encodedDigitCount++;
+            }
+
+            return value;
+        }
+
+    }
+}
---
+++ test/EncodeDotNet.Tests/Encode-Base32Tests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using EncodeDotNet;
+
+namespace EncodeDotNet.Tests
+{
+    public class EncodeBase32Tests
+    {
+
+        [Theory, MemberData(nameof(InterestingNumbers))]
+        public void EncodedValuesCanBeDecodedBackToOriginal(long number)
+        {
+            string withSeperator = Encode.ToBase32(number);
+            string noSeperator = Encode.ToBase32(number, false);
+
+            long actualWithSeperator = Encode.FromBase32(withSeperator);
+            long actualNoSeperator = Encode.FromBase32(noSeperator);
+            Assert.Equal(number, actualWithSeperator);
+            Assert.Equal(number, actualNoSeperator);
+        }
+
+        [Theory, MemberData(nameof(InterestingStrings))]
+        public void DecodedStringMatchesExpectValue(string input, long expectedValue)
+        {
+            long actual = Encode.FromBase32(input);
+            Assert.Equal(expectedValue, actual);
+        }
+
+        [Theory]
+        [InlineData(0, 12)]
+        [InlineData(100000, 12)]
+        [InlineData(0x0FFFFFFFFFFFFFFF, 12)]
+        [InlineData(0x1FFFFFFFFFFFFFFF, 13)]
+        [InlineData(0x7FFFFFFFFFFFFFFF, 13)]
+        public void EncodingUses12CharsForAllButTheLargestNumbers(long number, int expectedLength)
+        {
+            string base32 = Encode.ToBase32(number, false);
+            Assert.Equal(expectedLength, base32.Length);
+        }
+
+        [Fact]
+        public void DecodingThrowsForTooBigANumber()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Encode.FromBase32("8ZZZZ-ZZZZ-ZZZZ"));
+        }
+
+        private static IEnumerable<object[]> InterestingNumbers()
+        {
+            var numbers = new[] {0, 1, 1000, 183317821147582464, long.MaxValue};
+            return numbers.Select(x => new object[] { x });
+        }
+
+        private static IEnumerable<object[]> InterestingStrings()
+        {
+            return new[]
+            {
+                new object[] { "52TG-KSAW-0400", 183328850812342272 },
+                new object[] { "ZZZZ-ZZZZ-ZZZZ", 0x0FFFFFFFFFFFFFFF },   // largest 12 char value
+                new object[] { "1ZZZZ-ZZZZ-ZZZZ", 0x1FFFFFFFFFFFFFFF },  // 13 chars
+                new object[] { "7ZZZZ-ZZZZ-ZZZZ", long.MaxValue },       // 13 chars
+                new object[] { "1234-1101-ABCD", 38390583430294925 },    // normal characters
+                new object[] { "1234-ILO1-ABCD", 38390583430294925 },    // similar looking characters
+                new object[] { "1234-ilo1-abcd", 38390583430294925 }     // similar looking lowercase characters
+            };
+        }
+
+    }
+}
